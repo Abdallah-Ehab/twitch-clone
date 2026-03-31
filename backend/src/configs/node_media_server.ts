9b39e9195fedc9cdc6c188349eaa2a360a6d87ad
AbdallahEhab <abdallahehab3710@gmail.com>
2026-03-31 05:44:15 +0200

    feat: add prePublish, donePublish hooks to nms

--- backend/src/configs/node_media_server.ts
+++ backend/src/configs/node_media_server.ts
@@ -1,4 +1,5 @@
 import NodeMediaServer from 'node-media-server';
+import userModel from '../models/user.model.js';
 
 const config = {
   rtmp: {
@@ -32,3 +33,42 @@ const config = {
 
 var nms = new NodeMediaServer(config as any)
 nms.run();
+
+nms.on('prePublish', async (id, StreamPath, args) => {
+  console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  const streamKey = StreamPath.split('/').pop();
+  const session = nms.getSession(id) as any;
+  try{
+      const user = await userModel.findOne({ streamkey: streamKey });
+      if (!user) {
+        console.log('User not found. Rejecting stream.');
+        session?.reject();
+      }else{
+        user.updateOne({ isStreaming: true }).exec();
+        console.log(`Stream accepted for user: ${user.username}`);
+      }
+
+  }catch(err){
+    const session = nms.getSession(id) as any;
+    session?.reject();
+    console.error('Error occurred while fetching user:', err);
+  }
+});
+
+
+nms.on('donePublish', async (id, StreamPath, args) => {
+  console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  const streamKey = StreamPath.split('/').pop();
+  try{
+      const user = await userModel.findOne({ streamkey: streamKey });
+      if (user) {
+        user.updateOne({ isStreaming: false }).exec();
+        console.log(`Stream ended for user: ${user.username}`);
+      }else{
+        console.log('User not found for ended stream.');
+      }
+
+  }catch(err){
+    console.error('Error occurred while fetching user:', err);
+  }
+});
